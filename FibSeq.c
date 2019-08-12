c3bad8d4fe7afb2657a2056dcbbbbf18b6bf1240
TylerJSurroca <tylerjsurroca@gmail.com>
2019-08-11 22:37:39 -0400

    Error Update

--- FibSeq.c
+++ FibSeq.c
@@ -5,11 +5,8 @@
 
 unsigned long long g_fibNumbersWeveSeenSoFar[MAX_MEMORY];
 
-unsigned long long getTheFibonacciNumber(int theFibNumberThatWeWant)
+unsigned long long getTheFibonacciNumberMemoization(int theFibNumberThatWeWant)
 {
-    /*int fibSeq[] = {0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181};
-    return(fibSeq[n-1]);
-    */
 
     //printf("theFibNumberThatWeWant = %d\n",theFibNumberThatWeWant);
 
@@ -28,9 +25,38 @@ unsigned long long getTheFibonacciNumber(int theFibNumberThatWeWant)
     else
     {
         //Find the number with the Fibonacci algorithm and store it for later.
-        g_fibNumbersWeveSeenSoFar[theFibNumberThatWeWant] = getTheFibonacciNumber(theFibNumberThatWeWant-2) +
-                                                                                  getTheFibonacciNumber(theFibNumberThatWeWant-1);
+        g_fibNumbersWeveSeenSoFar[theFibNumberThatWeWant] = getTheFibonacciNumberMemoization(theFibNumberThatWeWant-2) +
+                                                                                  getTheFibonacciNumberMemoization(theFibNumberThatWeWant-1);
         return g_fibNumbersWeveSeenSoFar[theFibNumberThatWeWant];
+    }
+
+}
+
+unsigned long long getTheFibonacciNumberRecursive(int theFibNumberThatWeWant)
+{
+    if(theFibNumberThatWeWant == 0)
+        return 0;
+    if(theFibNumberThatWeWant == 1)
+        return 1;
+    if(theFibNumberThatWeWant == 2)
+        return 1;
+
+    return getTheFibonacciNumberRecursive(theFibNumberThatWeWant-2)+getTheFibonacciNumberRecursive(theFibNumberThatWeWant-1);
+
+}
+
+int getTheFibonacciNumberArray(int theFibNumberThatWeWant)
+{
+    int fibSeq[] = {0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181};
+
+    if(theFibNumberThatWeWant > 19)
+    {
+        printf("ERROR: NUMBER NOT AVAILABLE\n");
+        return NULL;
+    }
+    else
+    {
+        return(fibSeq[theFibNumberThatWeWant]);
     }
 
 }
@@ -46,29 +72,22 @@ int main()
     scanf("%d",&userInput);
 
     start = clock();
-    returnValue = getTheFibonacciNumber(userInput-1);
-    printf("Your Fibonacci number is: %llu\n",returnValue);
+    returnValue = getTheFibonacciNumberMemoization(userInput-1);
+    printf("Your Fibonacci number through Memoization is: %llu\n",returnValue);
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("Time Taken: %lf seconds\n",cpu_time_used);
 
     start = clock();
-    returnValue = getTheFibonacciNumber(userInput-1);
-    printf("Your Fibonacci number is: %llu\n",returnValue);
+    returnValue = getTheFibonacciNumberArray(userInput-1);
+    printf("Your Fibonacci number through an Array is: %d\n",returnValue);
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("Time Taken: %lf seconds\n",cpu_time_used);
 
     start = clock();
-    returnValue = getTheFibonacciNumber(userInput-1);
-    printf("Your Fibonacci number is: %llu\n",returnValue);
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("Time Taken: %lf seconds\n",cpu_time_used);
-
-    start = clock();
-    returnValue = getTheFibonacciNumber(userInput-1);
-    printf("Your Fibonacci number is: %llu\n",returnValue);
+    returnValue = getTheFibonacciNumberRecursive(userInput-1);
+    printf("Your Fibonacci number through Recursion is: %llu\n",returnValue);
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("Time Taken: %lf seconds\n",cpu_time_used);
